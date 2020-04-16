06335207994da6da70a564bd81bf9a776d62d016
LuizFernando55 <45644952+LuizFernando55@users.noreply.github.com>
2020-04-16 01:49:20 -0300

    add titulo; add rodape; add fontes e cores

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,13 +7,13 @@ function cabecalho() {
         <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="noticias.html">Noticias</a></li>
-                    <li class="nav-item"><a class="nav-link" href="sobre.html">Quem Somos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#contato">Contato</a></li>
+                    <li class="nav-item"><a class="nav-link" href="noticias.html">NOTÍCIAS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="sobre.html">QUEM SOMOS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contato">CONTATO</a></li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
         </nav>
